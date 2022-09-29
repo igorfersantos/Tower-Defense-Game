b7143bc6a2efffbec90c8c44512bbfa26244a4d1
Igor Fernandes <igorfercontato@gmail.com>
2022-09-29 11:37:45 -0300

    added readme, changes on missile launcher

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,9 +6,10 @@ public class Bullet : MonoBehaviour
     private Transform target;
 
     public float speed = 70f;
+    public float explosionRadious = 0f;
     public ParticleSystem impactEffect;
 
-    public void Seek (Transform _target)
+    public void Seek(Transform _target)
     {
         target = _target;
     }
@@ -16,7 +17,7 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -28,7 +29,7 @@ public class Bullet : MonoBehaviour
             return;
         }
 
-        
+
         Vector3 direction = target.position - transform.position;
         float distanceThisFrame = speed * Time.deltaTime;
 
@@ -39,14 +40,50 @@ public class Bullet : MonoBehaviour
         }
 
         transform.Translate(direction.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
     }
 
     private void HitTarget()
     {
         ParticleSystem effectInstance = Instantiate(impactEffect, transform.position, transform.rotation);
-        
+        Destroy(effectInstance, 5f);
+
+        if (explosionRadious > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
+
         Destroy(gameObject);
-        Destroy(target.gameObject);
-        Destroy(effectInstance, 2f);
+
+    }
+
+    private void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadious);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.tag == "Enemy")
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    private void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+
+    /// <summary>
+    /// Callback to draw gizmos only if the object is selected.
+    /// </summary>
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadious);
     }
 }
---
+++ README.md
@@ -0,0 +1,15 @@
+# Tower Defense Game
+
+A Tower Defense Game based on Brackey's playlist [How to make a Tower Defense Game](https://www.youtube.com/watch?v=beuoNuK2tbk&list=PLPV2KyIb3jR4u5jX8za5iU1cqnQPmbzG0) being made as a study case on Unity, currently on early stage
+
+![progress](https://cdn.discordapp.com/attachments/1022199681406738573/1022930365217779732/progresso_unity7.gif)
+
+## Build
+
+[Latest build](https://github.com/igorfersantos/Tower-Defense-Game/releases/tag/v0.0.1)
+
+## Contributing
+Pull requests and ideas are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
